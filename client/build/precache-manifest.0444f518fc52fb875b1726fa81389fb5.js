bc32b135775c941716eda580b18c97c0678d72eb
davidferro <david-ferro@sbcglobal.net>
2019-03-25 20:00:41 -0400

    build

---
+++ client/build/precache-manifest.0444f518fc52fb875b1726fa81389fb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc6a46659a6d0f779fccea66c6b0422d",
+    "url": "/static/media/Open-Book.dc6a4665.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cd45397b130f5bbc421b",
+    "url": "/static/js/main.083e56ad.chunk.js"
+  },
+  {
+    "revision": "ee9f5f76eefe15a665e5",
+    "url": "/static/js/2.ac999395.chunk.js"
+  },
+  {
+    "revision": "cd45397b130f5bbc421b",
+    "url": "/static/css/main.27f4e598.chunk.css"
+  },
+  {
+    "revision": "dde9447a697d379c7d919eb8a984fc1e",
+    "url": "/index.html"
+  }
+];
